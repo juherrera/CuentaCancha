a9a468021e8c4aa8316c817e1e3360039df06fc2
Julio Herrera <juherreramiranda@gmail.com>
2026-09-07 21:52:29 -0300

    Se agrega Jumper Park al listado

--- pages/AbrirCuenta.py
+++ pages/AbrirCuenta.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from datetime import time
 
-canchas_disponibles = ['Cancha 1','Cancha 2','Cancha 3','Cancha 4','Cancha 5']
+canchas_disponibles = ['Cancha 1','Cancha 2','Cancha 3','Cancha 4','Cancha 5', 'Jumper Park']
 
 def mostrar_formulario_cuenta():
     """Muestra el formulario 'Crear Cuenta' y agrega la transaccion al session_state cuando se envia."""
